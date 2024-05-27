1ecfad056c8259f3276a03303408755f48361948
FarrukhShahid <farrukh.shahid0@gmail.com>
2024-05-27 14:38:07 +0500

    show border countries in details page

--- src/pages/country-detail/index.js
+++ src/pages/country-detail/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, CardContent, Typography, Button, CircularProgress } from '@mui/material';
-import { getCountryByName } from '../../services/data';
+import { getCountryByCode, getCountryByName } from '../../services/data';
 import { ArrowBack } from '@mui/icons-material';
 import { ApiVersionContext } from '../../api-version-context';
 import { MODEL_DATA } from '../../utils/functions';
@@ -11,15 +11,21 @@ function CountryDetail() {
     const { apiVersion } = useContext(ApiVersionContext);
     const [country, setCountry] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [borderCountries, setBorderCountries] = useState([]);
+    const [laodingBorderCountries, setLaodingBorderCountries] = useState(false);
+    
+    
 
     useEffect(() => {
         if (name) {
             const fetchCountryDetails = async () => {
                 try {
+                    setCountry(null);
                     setLoading(true);
                     const response = await getCountryByName(name, apiVersion);
                     console.log('response', response);
-                    setCountry(MODEL_DATA(response.data[0], parseFloat(apiVersion)));
+                    const parsedData = MODEL_DATA(response.data[0], parseFloat(apiVersion))
+                    setCountry(parsedData);
                 } catch (error) {
                     console.log('Error fetching country details:', error);
                 } finally {
@@ -30,15 +36,34 @@ function CountryDetail() {
         }
     }, [name, apiVersion]);
 
+    useEffect(() => {
+        if (country) {
+            const fetchBorderCountries = async () => {
+                try {
+                    setLaodingBorderCountries(true);
+                    const responseBorderCoutnries = await getCountryByCode(country.borders, apiVersion)
+                    console.log('responseBorderCoutnries', responseBorderCoutnries)
+                    setBorderCountries(responseBorderCoutnries.data);
+                } catch (error) {
+                    console.log('error', error);
+                }
+                finally {
+                    setLaodingBorderCountries(false);
+                }
+            }
+            fetchBorderCountries();
+        }
+    }, [country, apiVersion])
+
     if (loading) return <CircularProgress />;
 
     return (
         <div className="container mx-auto p-4">
-            <Button LinkComponent={Link} to="/" variant="contained" color='inherit' className='shadow-dm' startIcon={<ArrowBack />}>Back</Button>
+            <Button LinkComponent={Link} to="/" variant="contained"  className='shadow-dm' startIcon={<ArrowBack />}>Back</Button>
             {
                 country ?
                     <Card className="flex flex-col lg:flex-row items-start mt-4">
-                        <img src={country.flag} alt={country.name} className="w-full lg:w-1/2 h-auto object-cover mb-4 lg:mb-0" />
+                        <img src={country.flag} alt={country.name} className="w-full lg:w-1/2 h-auto object-fit mb-4 lg:mb-0" />
                         <CardContent className="lg:ml-8">
                             <Typography variant="h4" component="div" className="mb-4">{country.name}</Typography>
                             <Typography variant="body1"><strong>Native Name:</strong> {country.nativeName}</Typography>
@@ -49,7 +74,11 @@ function CountryDetail() {
                             <Typography variant="body1"><strong>Top Level Domain:</strong> {country.topLevelDomain.join(', ')}</Typography>
                             <Typography variant="body1"><strong>Currencies:</strong> {country.currencies.map(c => c.name).join(', ')}</Typography>
                             <Typography variant="body1"><strong>Languages:</strong> {country.languages.toString()}</Typography>
-                            <Typography variant="body1"><strong>Border Countries:</strong> {country.borders ? country.borders.join(', ') : 'None'}</Typography>
+                            <div className={`mt-5`}>
+                                <Typography variant="body1"><strong>Border Countries:</strong> { laodingBorderCountries? <CircularProgress />:
+                                    country.borders ? country.borders.join(', ') : 'None'}</Typography>
+
+                            </div>
                         </CardContent>
                     </Card> :
                     <div>No Data Found</div>
--- src/services/data.js
+++ src/services/data.js
@@ -7,3 +7,7 @@ export const getCountriesList = async (filters, version = 2) => {
 export const getCountryByName = async (name, version = 2) => {
     return await axios.get(`https://restcountries.com/v${version}/name/${name}`)
 }
+
+export const getCountryByCode = async (codes, version = 2) => {
+    return await axios.get(`https://restcountries.com/v${version}/alpha?codes=${Array.isArray(codes) ? codes.join(",") : codes}`)
+}
